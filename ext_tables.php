8aad1fa4d51915beea04f26472794cf6d140ab7a
25pixel <info@25pixel.de>
2025-05-19 17:18:09 +0200

    Update ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -3,4 +3,4 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('presentation_package', 'Configuration/TypoScript', 'Kitodo Presentation Package');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('kitodo_presentation_template', 'Configuration/TypoScript', 'Kitodo Presentation Template');
