e54997d6404963c66207f58a5e666fa4b17900cb
25pixel <info@25pixel.de>
2025-05-19 17:10:03 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,23 +1,22 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Kitodo Presentation Package',
-	'description' => 'Custom template for Kitodo.Presentation, developed at the Sorbian Institute (Serbski Institut)',
-	'category' => 'distribution',
-	'author' => 'Tobias Urland-Wocko',
-	'author_email' => 'tobias.urland-wocko@serbski-institut.de',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => '8.7',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '8.7-9.9.99',
-			'flux' => '8-9.9',
-		),
-		'suggests' => array(
-		),
-	),
+    'title' => 'Kitodo Presentation Template',
+    'description' => 'Custom template for Kitodo.Presentation, developed at the Sorbian Institute (Serbski Institut)',
+    'category' => 'templates',
+    'author' => 'Tobias Urland-Wocko',
+    'author_email' => 'tobias.urland-wocko@serbski-institut.de',
+    'state' => 'stable',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'version' => '1.0.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '10.4.0-11.5.99',
+            'flux' => '',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
 );
