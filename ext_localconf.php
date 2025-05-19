acf76af28a470c59b840786a673adfab8ad92a23
25pixel <info@25pixel.de>
2025-05-19 17:17:31 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,16 +1,14 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
-}
+defined('TYPO3_MODE') || die('Access denied.');
 
-\FluidTYPO3\Flux\Core::registerProviderExtensionKey('Kitodo.PresentationPackage', 'Content');
-\FluidTYPO3\Flux\Core::registerProviderExtensionKey('Kitodo.PresentationPackage', 'Page');
+\FluidTYPO3\Flux\Core::registerProviderExtensionKey('SerbskiInstitut.KitodoPresentationTemplate', 'Content');
+\FluidTYPO3\Flux\Core::registerProviderExtensionKey('SerbskiInstitut.KitodoPresentationTemplate', 'Page');
 
-//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('presentation_package','Configuration/TypoScript', 'Presentation Package');
+//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('kitodo_presentation_template','Configuration/TypoScript','Kitodo Presentation Template');
 
 // Override language files
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:dlf/Resources/Private/Language/locallang_metadata.xlf'][] = 'EXT:presentation_package/Resources/Private/Language/Overrides/locallang_metadata.xlf';
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:dlf/Resources/Private/Language/locallang_metadata.xlf'][] = 'EXT:presentation_package/Resources/Private/Language/Overrides/de.locallang_metadata.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:dlf/Resources/Private/Language/locallang_metadata.xlf'][] = 'EXT:kitodo_presentation_template/Resources/Private/Language/Overrides/locallang_metadata.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:dlf/Resources/Private/Language/locallang_metadata.xlf'][] = 'EXT:kitodo_presentation_template/Resources/Private/Language/Overrides/de.locallang_metadata.xlf';
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:dlf/Resources/Private/Language/locallang_structure.xlf'][] = 'EXT:presentation_package/Resources/Private/Language/Overrides/locallang_structure.xlf';
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:dlf/Resources/Private/Language/locallang_structure.xlf'][] = 'EXT:presentation_package/Resources/Private/Language/Overrides/de.locallang_structure.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:dlf/Resources/Private/Language/locallang_structure.xlf'][] = 'EXT:kitodo_presentation_template/Resources/Private/Language/Overrides/locallang_structure.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:dlf/Resources/Private/Language/locallang_structure.xlf'][] = 'EXT:kitodo_presentation_template/Resources/Private/Language/Overrides/de.locallang_structure.xlf';
